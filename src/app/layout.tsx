6e0fdff6efc8b29f11579ea5d9d4755e187a838a
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2025-08-21 19:50:12 +0100

    Clean up initial nextjs code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,10 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} antialiased`}>
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <h1 className="text-4xl font-bold">Welcome to Meteo Ville</h1>
+          {children}
+          <footer className="text-sm text-gray-500">
+            © {new Date().getFullYear()} Meteo Ville. All rights reserved.
+          </footer>
+        </div>
       </body>
     </html>
   );
