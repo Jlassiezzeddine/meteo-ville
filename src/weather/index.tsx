2891eb4edf803babf68e89980c4cf2746b6cf1a7
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2025-08-25 03:21:54 +0100

    Fix layout

--- src/weather/index.tsx
+++ src/weather/index.tsx
@@ -59,81 +59,82 @@ function WeatherWidget() {
         options={comboboxOptions}
         isLoading={isFetching}
       />
-      {selected && (
-        <div className="flex items-center">
-          {weather && (
-            <Badge variant="secondary" className="rounded-full">
-              {new Date(weather.date * 1000).toLocaleDateString(undefined, {
-                weekday: "long",
-                day: "2-digit",
-                month: "long",
-              })}
-            </Badge>
-          )}
-        </div>
-      )}
 
       {weather && selected && (
-        <Card className="p-8 text-sm">
-          <div>
-            <div className="relative h-40 w-32">
-              <Image
-                src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
-                alt={weather.main ?? "Weather icon"}
-                fill
-                style={{ objectFit: "cover" }}
-              />
+        <>
+          <Badge variant="secondary" className="rounded-full">
+            {new Date(weather.date * 1000).toLocaleDateString(undefined, {
+              weekday: "long",
+              day: "2-digit",
+              month: "long",
+            })}
+          </Badge>
+          <Card className="p-8 text-sm">
+            <div>
+              <div className="relative h-40 w-32">
+                <Image
+                  src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
+                  alt={weather.main ?? "Weather icon"}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <p className="text-8xl">
+                {Math.round(weather.temperature ?? 0)}°
+              </p>
+              <p className="text-sm font-bold capitalize">
+                {weather.description}
+              </p>
+              <p className="text-xs">
+                {weather.feelsLike !== weather.temperature &&
+                  ` Feels like ${weather.feelsLike ?? 0}°`}
+                <br />
+                Sunrise will be at{" "}
+                {new Date(weather.sunrise * 1000).toLocaleTimeString(
+                  undefined,
+                  {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  },
+                )}{" "}
+                <br />
+                Sunset will be at{" "}
+                {new Date(weather.sunset * 1000).toLocaleTimeString(undefined, {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
             </div>
-            <p className="text-8xl">{Math.round(weather.temperature ?? 0)}°</p>
-            <p className="text-sm font-bold capitalize">
-              {weather.description}
-            </p>
-            <p className="text-xs">
-              {weather.feelsLike !== weather.temperature &&
-                ` Feels like ${weather.feelsLike ?? 0}°`}
-              <br />
-              Sunrise will be at{" "}
-              {new Date(weather.sunrise * 1000).toLocaleTimeString(undefined, {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}{" "}
-              <br />
-              Sunset will be at{" "}
-              {new Date(weather.sunset * 1000).toLocaleTimeString(undefined, {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </p>
-          </div>
 
-          <Card className="grid grid-cols-3 gap-3 p-4">
-            <div className="flex flex-col items-center justify-center gap-2 text-center">
-              <Icon icon="wind" className="mx-auto size-7" />
-              <div>
-                <p className="text-sm font-semibold">
-                  {Math.round(weather.windSpeed ?? 0)} km/h
-                </p>
-                <p className="text-muted-foreground text-xs">Wind</p>
+            <Card className="bg-primary grid grid-cols-3 gap-3 p-4">
+              <div className="flex flex-col items-center justify-center gap-2 text-center">
+                <Icon icon="wind" className="mx-auto size-7" />
+                <div>
+                  <p className="text-sm font-semibold">
+                    {Math.round(weather.windSpeed ?? 0)} km/h
+                  </p>
+                  <p className="text-muted-foreground text-xs">Wind</p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-2 text-center">
-              <Icon icon="droplet" className="mx-auto size-7" />
-              <div>
-                <p className="text-sm font-semibold">{weather.humidity}%</p>
-                <p className="text-muted-foreground text-xs">Humidity</p>
+              <div className="flex flex-col items-center justify-center gap-2 text-center">
+                <Icon icon="droplet" className="mx-auto size-7" />
+                <div>
+                  <p className="text-sm font-semibold">{weather.humidity}%</p>
+                  <p className="text-muted-foreground text-xs">Humidity</p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-2 text-center">
-              <Icon icon="eye" className="mx-auto size-7" />
-              <div>
-                <p className="text-sm font-semibold">
-                  {(weather.visibility ?? 0) / 1000}km
-                </p>
-                <p className="text-muted-foreground text-xs">Visibility</p>
+              <div className="flex flex-col items-center justify-center gap-2 text-center">
+                <Icon icon="eye" className="mx-auto size-7" />
+                <div>
+                  <p className="text-sm font-semibold">
+                    {(weather.visibility ?? 0) / 1000}km
+                  </p>
+                  <p className="text-muted-foreground text-xs">Visibility</p>
+                </div>
               </div>
-            </div>
+            </Card>
           </Card>
-        </Card>
+        </>
       )}
     </div>
   );
